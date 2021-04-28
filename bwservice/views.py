37a2cd609a9ffdea33eb8b75f1c55abee6ec87ac
haozipu <zipu405@126.com>
2021-04-28 14:26:02 +0800

    modify

--- bwservice/views.py
+++ bwservice/views.py
@@ -4,6 +4,7 @@ import os
 from blind_watermark import WaterMark
 from django.http import HttpResponse
 from django.shortcuts import render
+import cv2 as cv
 
 
 def hello(request):
@@ -39,9 +40,14 @@ def savefile(request):
         bwm1.embed('static/uploadfiles/bwfiles/' + file_obj.name)
         len_wm = len(bwm1.wm_bit)
         print('Put down the length of wm_bit {len_wm}'.format(len_wm=len_wm))
+        img = cv.imread('static/uploadfiles/bwfiles/' + file_obj.name)
+        # 降噪处理
+        dst = cv.fastNlMeansDenoisingColored(img, None, 7, 7, 7, 21)
+        cv.imwrite('static/uploadfiles/sfiles/' + file_obj.name, dst, params=None)
 
         data = {
             'srcImg': '/static/uploadfiles/' + file_obj.name,
-            'desImg': '/static/uploadfiles/bwfiles/' + file_obj.name
+            'desImg': '/static/uploadfiles/bwfiles/' + file_obj.name,
+            'secImg': '/static/uploadfiles/sfiles/' + file_obj.name
         }
-    return HttpResponse(json.dumps(data),content_type="application/json")
+    return HttpResponse(json.dumps(data), content_type="application/json")
