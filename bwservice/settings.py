6ad0839a72bc2c09bd92eabd06a0abf2a127ce88
haozipu <zipu405@126.com>
2021-05-11 17:17:45 +0800

    modify

--- bwservice/settings.py
+++ bwservice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3@euks5d_b3)$l5^50xhn)hdkw5bi&oua@ldv6lck6b!=9mq@u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['*']
 
 
 # Application definition
