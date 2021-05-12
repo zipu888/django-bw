10b73a5482b449770a65ff5fceb6c5b57cf0aa7b
haozipu <zipu405@126.com>
2021-05-12 16:07:08 +0800

    modify

--- bwservice/settings.py
+++ bwservice/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3@euks5d_b3)$l5^50xhn)hdkw5bi&oua@ldv6lck6b!=9mq@u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
